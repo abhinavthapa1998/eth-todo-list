2464d64be40ebde913af0c1368bbd9789b8ba5bb
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-01-02 22:14:54 +0530

    Toggle Functionality and Test

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -33,4 +33,13 @@ contract("TodoList", function (accounts) {
     assert.equal(event.content, "Study Blockchain Basics");
     assert.equal(event.completed, false);
   });
+
+  it("toggles task completion", async () => {
+    const result = await this.todoList.toggleCompleted(1);
+    const task = await this.todoList.tasks(1);
+    assert.equal(taskCompleted, true);
+    const event = result.logs[0].args;
+    assert.equal(event.id.toNumber(), 1);
+    assert.equal(event.completed, true);
+  });
 });
